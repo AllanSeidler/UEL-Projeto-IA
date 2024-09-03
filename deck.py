2c01d26daaf027540e16794dedde5ec772560cdd
AllanSeidler <allanseidler@gmail.com>
2024-09-02 22:19:25 -0300

    Criação das heuristicas

--- deck.py
+++ deck.py
@@ -4,22 +4,27 @@ Cria um deck de tamanho n*52 cartas
 import random as rdm
 
 class Deck:
-    def __init__(self, n:int):
+    def __init__(self):
         self.cards = []
-        self.counter = [4*n]*9
-        self.counter.append(16*n)
-
-        # suits=['♦','♠','♥','♣']
-        for j in range(self.counter[0]):
-            self.cards.append(11)
-        for i in range(1,10):
-            for j in range(self.counter[i]):
-                self.cards.append(i+1)
-        rdm.shuffle(self.cards)
-
+        self.size = 0
     
     '''Retira a carta no topo do deck e a retorna.'''
     def draw(self) -> int:
-        card = self.cards.pop()
-        self.counter[card%11]-=1
+        card = self.cards.pop(0)
+        self.counter[(card-1)%10]-=1
+        self.size -= 1
         return card
+
+    
+    def reset(self,n):
+        self.size= n*52
+        self.cards.clear()
+
+        self.counter = [4*n]*9
+        self.counter.append(16*n)
+
+        self.cards.extend([11]*self.counter[0])
+        for i in range(1,10):
+            self.cards.extend([i+1]*self.counter[i])
+
+        rdm.shuffle(self.cards)
--- heuristic.py
+++ heuristic.py
@@ -8,10 +8,62 @@ As funções de heuristica devem seguir o seguinte modelo:
     Onde v é o valor da mão e d um deck contendo cartas.
 '''
 from deck import Deck
-
+import statistics
 
 '''Um dealer que puxa cartas até ter 17 ou mais pontos na mão.'''
-def true_dealer(v:int, d:Deck) -> bool:
+def dealer_verdadeiro(v:int, d:Deck) -> bool:
     return v<17
 
+'''Se tem chance de perder, desiste.'''
+def medrosa(v:int, d:Deck) -> bool:
+    i = 9
+    c = d.counter[i]
+    while c==0:
+        i-=1
+        c = d.counter[i]
+    return (v+i)<22
+
+'''Se tem chance de aumentar o valor da mão, puxa uma carta.'''
+def corajosa(v:int, d:Deck) -> bool:
+    i = 0
+    c = d.counter[i]
+    while c==0:
+        i+=1
+        c = d.counter[i]
+    return (v+i)<22
+
+'''É um meio termo entre a corajosa e o dealer verdadeiro'''
+def dealer_falso(v:int, d:Deck) -> bool:
+    if v>17:
+        i = 0
+        c = d.counter[i]
+        while c==0:
+            i+=1
+            c = d.counter[i]
+        return ((v+i)<22)
+    else:
+        return False
+
+'''Vê a carta do topo do baralho.'''
+def trapaceiro(v:int, d:Deck) -> bool:
+    return (d.cards[0]+v)<22
+
+'''Faz estimativa por media.'''
+def media(v:int, d:Deck) -> bool:
+    soma = 0
+    for i in range(10):
+        soma+=i*d.counter[i]
+    return (v+soma/d.size) <= 21
+
+'''Faz estimativa por mediana.'''
+def mediana(v:int, d:Deck) -> bool:
+    return (v+statistics.median(d.cards)) <= 21
+
+'''Faz estimativa por moda.'''
+def moda(v:int, d:Deck) -> bool:
+    c = 0
+    for i in range(10):
+        if d.counter[i] > d.counter[c]:
+            c=i
+    return (c+1+v)<22    
 
